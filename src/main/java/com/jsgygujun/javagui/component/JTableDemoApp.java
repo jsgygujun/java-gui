bce2d2004b63ad8701742b42ca816f4295d370e8
jun <jsgygujun@gmail.com>
2020-10-25 23:03:40 +0800

    [ADD] ComboBoxDemoApp

---
+++ src/main/java/com/jsgygujun/javagui/component/JTableDemoApp.java
@@ -0,0 +1,13 @@
+package com.jsgygujun.javagui.component;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author GuJun
+ * @date 2020/10/25
+ */
+public class JTableDemoApp {
+
+    private final JFrame frame = new JFrame("简单表格");
+}
